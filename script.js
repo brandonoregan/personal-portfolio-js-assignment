6226d8aa8cf59fdfc54fc0260b9d091f06dd7ced
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-05-20 15:36:53 +0100

    new design

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const projectTiles = document.querySelectorAll(".project__container");
+
+projectTiles.forEach(function (container) {
+  container.addEventListener("click", function () {
+    container.classList.toggle("project__tile--bigger");
+  });
+});
