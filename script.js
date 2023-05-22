3eee3e7b36e7923d5539d2e9bf28cf589a282be5
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-05-22 16:02:00 +0100

    html css slider conversion  - no flex

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const aboutSection = document.getElementById("aboutSection");
 const projectsSection = document.getElementById("projectsSection");
 const contactSection = document.getElementById("contactSection");
 const navbar = document.querySelector(".navbar");
+
 // Add a class that increase tile size
 projectTiles.forEach(function (container) {
   container.addEventListener("click", function () {
@@ -14,6 +15,9 @@ projectTiles.forEach(function (container) {
   });
 });
 
+//EVENTS LISTENERS
+//EVENTS LISTENERS
+//EVENTS LISTENERS
 // Button that smoothy scrolls to contact sections
 heroButton.addEventListener("click", function () {
   contactSection.scrollIntoView({ behavior: "smooth" });
@@ -21,9 +25,6 @@ heroButton.addEventListener("click", function () {
 
 // event listener for nav tabs to smooth scroll
 navbar.addEventListener("click", function (e) {
-  if (e.target.closest("li").innerHTML === "HOME") {
-    homeSection.scrollIntoView({ behavior: "smooth" });
-  }
   if (e.target.closest("li").innerHTML === "ABOUT") {
     aboutSection.scrollIntoView({ behavior: "smooth" });
   }
@@ -33,4 +34,39 @@ navbar.addEventListener("click", function (e) {
   if (e.target.closest("li").innerHTML === "CONTACT") {
     contactSection.scrollIntoView({ behavior: "smooth" });
   }
+});
+
+const tiles = document.querySelectorAll(".projects--tile");
+const btnRight = document.querySelector(".btn--right");
+const btnLeft = document.querySelector(".btn--left");
+const projectsContainer = document.querySelector(".projects__container");
+
+let currentTile = 0;
+const maxTile = tiles.length;
+
+// tiles.forEach((tile, i) => (tile.style.transform = `translateX(${100 * i})%`));
+
+btnRight.addEventListener("click", function () {
+  console.log("hey");
+  if (currentTile === maxTile - 1) {
+    currentTile = 0;
+  } else {
+    currentTile++;
+  }
+  tiles.forEach(
+    (tile, i) =>
+      (tile.style.transform = `translateX(${300 * (i + currentTile)}px)`)
+  );
+});
+
+btnLeft.addEventListener("click", function () {
+  //   if (currentTile === 0) {
+  //     currentTile = maxTile - 1;
+  //   } else {
+  //     currentTile--;
+  //   }
+  tiles.forEach(
+    (tile, i) =>
+      (tile.style.transform = `translateX(${-300 * (i + currentTile)}px)`)
+  );
 });
